84450671f440e8afb384df31a7fbacca504dec18
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "route_tables" {
-  description = "All route_table resources"
-  value       = azurerm_route_table.route_tables
-}
 output "route_tables_bgp_route_propagation_enabled" {
-  description = "List of bgp_route_propagation_enabled values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.bgp_route_propagation_enabled]
+  description = "Map of bgp_route_propagation_enabled values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.bgp_route_propagation_enabled }
 }
 output "route_tables_location" {
-  description = "List of location values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.location]
+  description = "Map of location values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.location }
 }
 output "route_tables_name" {
-  description = "List of name values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.name]
+  description = "Map of name values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.name }
 }
 output "route_tables_resource_group_name" {
-  description = "List of resource_group_name values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.resource_group_name]
+  description = "Map of resource_group_name values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.resource_group_name }
 }
 output "route_tables_route" {
-  description = "List of route values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.route]
+  description = "Map of route values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.route }
 }
 output "route_tables_subnets" {
-  description = "List of subnets values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.subnets]
+  description = "Map of subnets values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.subnets }
 }
 output "route_tables_tags" {
-  description = "List of tags values across all route_tables"
-  value       = [for k, v in azurerm_route_table.route_tables : v.tags]
+  description = "Map of tags values across all route_tables, keyed the same as var.route_tables"
+  value       = { for k, v in azurerm_route_table.route_tables : k => v.tags }
 }
 
